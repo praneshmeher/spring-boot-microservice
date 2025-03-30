11666e4b439e8fffafcc8b3325de1fb9bc5665ba
praneshmeher <praneshmeher@gmail.com>
2025-03-30 12:24:48 +0530

    accounts service

--- src/main/java/com/bank/accounts/config/AccountsConfigurationProperties.java
+++ src/main/java/com/bank/accounts/config/AccountsConfigurationProperties.java
@@ -1,11 +1,19 @@
 package com.bank.accounts.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsConfigurationProperties(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
-
+@Getter
+@Setter
+@ToString
+public class AccountsConfigurationProperties {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
